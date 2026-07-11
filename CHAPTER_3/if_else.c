6d681436bd44cb72444a2b8561e169519d887163
Rudranil07 <rudranilghoshrock@gmail.com>
2026-07-11 19:54:56 +0530

    CHAPTER3_IF_ELSE

---
+++ CHAPTER_3/if_else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int age = 5;
+
+    if(age>=10){
+        printf("Your age is greater than or equal to 10");
+    }
+    else{
+        printf("Your age is not greater than or equal to 10");
+    }
+    return 0;
+}
