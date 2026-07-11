441dec32e4bab08389bff53f7cde49ab042a00cb
Rudranil07 <rudranilghoshrock@gmail.com>
2026-07-11 12:10:49 +0530

    CHAPTER 1 DONE

--- CHAPTER_1/Q4.c
+++ CHAPTER_1/Q4.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     float p = 600.25;
     float r = 1.2;
     float t = 3;
-    printf("Simple Interest with the data\n p = %f\n r = %f\n t=%f\n is %f", p,r,t,p*r*t);
+    printf("Simple Interest with the data\n p = %f\n r = %f\n t=%f\n is %f", p, r, t, p * r * t);
     return 0;
 }
