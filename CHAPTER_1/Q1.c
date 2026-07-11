d28abe91ae0d15870b17673ab0e40a2e5a00f02d
Rudranil07 <rudranilghoshrock@gmail.com>
2026-07-11 10:51:20 +0530

    CH1 Q1 Done

---
+++ CHAPTER_1/Q1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int length,breadth;
+
+    printf("Enter length\n");;
+    scanf("%d",&length);
+
+    printf("Enter breadth\n");
+    scanf("%d",&breadth);
+
+    printf("Area of rectangle is %d\n",length*breadth);
+
+
+    return 0;
+}
