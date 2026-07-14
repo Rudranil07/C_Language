4a802ac3c3de198b85dbc69db75d23df1fe8eaf0
Rudranil07 <rudranilghoshrock@gmail.com>
2026-07-14 23:44:53 +0530

    Chapter 5 halfway

---
+++ CHAPTER_5/quick.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void gm(){
+    printf("Good Morning\n");
+}
+void ga(){
+    printf("Good Afternoon\n");
+}
+void gn(){
+    printf("Good Night\n");
+}
+int main(){
+    gm();
+    ga();
+    gn();
+    return 0;
+}
